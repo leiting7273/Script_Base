97e4ceb8b9366e68d785efb7a9d6130816c94755
卷残云 <45893872+leiting7273@users.noreply.github.com>
2023-12-04 20:36:48 +0800

    更新

--- B站/bilibili直播签到_金山文档.js
+++ B站/bilibili直播签到_金山文档.js
@@ -1,5 +1,19 @@
+/**
+ * 序言：本脚本用于B站直播自动签到以及大会员每月自动领B币（暂时不会自动兑换瓜子，月底前记得用掉）
+ */
 const biliSheet = Application.Sheets("BiliBili")  //Bilibili工作表
-const emailSheet = Application.Sheets("发信邮箱配置")   //发信邮箱配置工作表
+if (biliSheet == null) {
+  console.error('未找到表格“BiliBili”')
+  return
+}
+const msgSheet = Application.Sheets("【通知消息】") //【通知消息】工作表
+if (msgSheet == null) {
+  console.error('未找到表格“【通知消息】”')
+  return
+}
+var msgResult = ''
+var msgContent = ''
+
 const myDate = new Date() //获取当前时间
 const currentDate = myDate.getFullYear() + "-" + (myDate.getMonth() + 1) + "-" + myDate.getDate()  //提取当日日期
 const dateNum = myDate.getFullYear() * 10000 + (myDate.getMonth() + 1) * 100 + myDate.getDate()
@@ -7,66 +21,103 @@ const tomorrow = new Date(myDate.getTime() + 24 * 60 * 60 * 1000)
 const tomorrowNum = tomorrow.getFullYear() * 10000 + (tomorrow.getMonth() + 1) * 100 + tomorrow.getDate()
 const last = new Date(myDate.getFullYear(), myDate.getMonth() + 1, 0).getDate()//本月天数
 
-doCheckIn()//执行签到
+//执行签到
+doCheckIn()
+
+//写入消息日志
+msgSheet.Range('B3').Value = msgResult;
+msgSheet.Range('C3').Value = msgContent;
+msgSheet.Range('D3').Value = myDate.getFullYear() + '-' + (myDate.getMonth() + 1).toString().padStart(2, '0') + '-' + myDate.getDate().toString().padStart(2, '0');
+
+
+/**
+ * 以下为函数实现
+ * 
+ */
 
 //遍历签到
 function doCheckIn() {
-  var bliCookie, isSignIn, lastDay, isSendEmail, userEmail, isSendWeChat, pushToken, is_Vip, month_ReDay, _csrf
+  var bliCookie, isSignIn, lastDay, is_Vip, month_ReDay, _csrf
   for (let row = 2; "" != (isSignIn = biliSheet.Range("E" + row).Text); row++) {
     bliCookie = biliSheet.Range("A" + row).Text  //用户凭据
-    //isSignIn = biliSheet.Range("E" + row).Text  //是否自动签到
     lastDay = biliSheet.Range("G" + row).Text   //签到日期
-    isSendEmail = biliSheet.Range("I" + row).Text //是否发送邮箱签到提醒
-    userEmail = biliSheet.Range("L" + row).Text //接收邮箱地址
-    isSendWeChat = biliSheet.Range("O" + row).Text  //是否发送微信公众号签到提醒
-    pushToken = biliSheet.Range("R" + row).Text //pushplus_token
-    is_Vip = biliSheet.Range("W" + row)
-    month_ReDay = biliSheet.Range("Y" + row)
-    _csrf = biliSheet.Range("Z" + row)
+    is_Vip = biliSheet.Range("I" + row)
+    month_ReDay = biliSheet.Range("K" + row)
+    _csrf = biliSheet.Range("L" + row)
 
     if (isSignIn != "是") continue //不签到的用户跳过
     if (lastDay == currentDate) { //已签到的用户跳过
-      console.log("A" + row + "用户今日已签到，无需重复签到！")
+      addMsg("A" + row + "用户今日已签到，无需重复签到！")
+      msgResult = '✅'
       continue
     }
     let res = signIn(bliCookie)  //执行用户签到
-    let msg = getMsg(row, res) //获取通知信息
+    addMsg(getInfo(bliCookie).uname + getMsg(row, res)) //获取通知信息
 
     if (month_ReDay.Text == "" || dateNum - month_ReDay.Value > last) {   //每月检查是否是年度大会员
       let isvip = getUserVip(bliCookie)
-      if (!isvip) continue //本月没开会员，跳过
-      is_Vip.Value = "是"
-      _csrf.Value = getCsrf(bliCookie)  //读出cookie中的csrf令牌
-      let havbq = havBq(bliCookie)
-      if (havbq[0].state == 0) {  //查询是否有未领取的B币券
-        let res = getBq(bliCookie, 1)  //领B币券
-        for (let i = 2; i <= havbq.length; i++) {  //领其它福利
-          getBq(bliCookie, i)
-        }
-        if (res.code == 0) {
-          console.log("用户A" + row + "领取B币券成功")
-          msg += "|领取本月B币券成功"
-          month_ReDay.Value = dateNum
-        } else {
-          console.log("用户A" + row + res.message)
-          msg += "|领取本月B币券结果：" + res.message
-        }
-      } else if (month_ReDay.Value+100<tomorrowNum&&dateNum<month_ReDay.Value+100) {  //过期前1天
+      if (isvip) { //本月没开会员，跳过
+        is_Vip.Value = "是"
+        _csrf.Value = getCsrf(bliCookie)  //读出cookie中的csrf令牌
+        let havbq = havBq(bliCookie)
+        if (havbq[0].state == 0) {  //查询是否有未领取的B币券
+          let res = getBq(bliCookie, 1)  //领B币券
+          for (let i = 2; i <= havbq.length; i++) {  //领其它福利
+            getBq(bliCookie, i)
+          }
+          if (res.code == 0) {
+            console.log("用户A" + row + "领取B币券成功")
+            addMsg("|领取本月B币券成功")
+            month_ReDay.Value = dateNum
+          } else {
+            console.log("用户A" + row + res.message)
+            addMsg("|领取本月B币券结果：" + res.message)
+          }
+        } else if (month_ReDay.Value + 100 < tomorrowNum && dateNum < month_ReDay.Value + 100) {  //过期前1天
           let balance = getUserWallet(bliCookie)
           if (balance > 0) {
             //兑换电池,暂未实现,仅提示
-            sendWeChat(pushToken, "B币券即将过期【重要提醒】", "<font color=Red>**你的B币券余额"+balance+"还有不到一天就要过期啦**</font>")
+            addMsg("| B币券即将过期【重要提醒】" + "你的B币券余额" + balance + "还有不到一天就要过期啦")
           }
         }
+      }
     }
 
-    /**发送签到结果给用户 */
-    if (isSendEmail != "是") { console.log("用户A" + row + "不发送邮件通知") } //是否邮件通知
-    else { sendEmail(userEmail, msg) }
-
-    if (isSendWeChat != "是") { console.log("用户A" + row + "不发送微信通知") }  //是否微信通知
-    else { sendWeChat(pushToken, "Bilibili签到通知<" + currentDate + ">", msg) }
   }
+}
+
+//获取用户个人信息
+function getInfo(bliCookie) {
+  let msg = ""
+  let headers = { "cookie": bliCookie }
+  let result = HTTP.get("https://api.bilibili.com/x/space/myinfo", { headers })
+  if (result.status !== 200) { throw new Error("fetch err! status is " + resp.status()) }//服务器响应错误
+  let reg = result.json()
+  //console.log("------------------------------------")
+  if (reg.code == -101) {
+    msg = "cookie已失效"
+    console.log(msg)
+    return msg
+  }
+  let uid = reg.data.mid //用户id
+  let uname = reg.data.name //用户名
+  let level = reg.data.level_exp.current_level  //用户等级
+  let current_exp = reg.data.level_exp.current_exp  //当前经验值
+  let next_exp = reg.data.level_exp.next_exp  //下一级经验值
+  let need_exp = next_exp - current_exp //升到下一级，需要的经验值
+  let rate = (current_exp / next_exp * 100).toFixed(2) + '%'; //进度
+  let coin = reg.data.coins //硬币数 
+  msg = "用户id：" + uid + " 用户名：" + uname + " 等级：" + level + "，硬币数：" + coin + "，当前经验值：" + current_exp + "，升级还需" + need_exp + "，当前进度：" + rate
+  result = {
+    'uid': uid,
+    'uname': uname,
+    'level': level,
+    'coin': coin,
+    'current_exp': current_exp,
+    'need_exp': need_exp,
+    'rate': rate
+  }
+  return result
 }
 
 //生成订单
@@ -196,51 +247,32 @@ function getMsg(row, res) {
   let msg = ""  //待推送给用户的信息
   switch (code) { //解析code
     case 0: msg = "今日签到成功，获取奖励：" + res.data.text
+      msgResult = '✅'
       updateDate(row, currentDate)
       break
     case -101: msg = "未登录，请更新COOKIE！"
+      msgResult = '❌'
       break
-    case 1011040: msg = res.message
+    case 1011040:
+      msgResult = '❓'
+      msg = res.message
       break
-    default: msg = "签到发生未知异常"
+    default:
+      msgResult = '❌'
+      msg = "签到发生未知异常"
   }
   console.log("用户A" + row + msg)
   return msg
 }
 
-//微信通知
-function sendWeChat(pushToken, title, content) {
-  let url = "http://www.pushplus.plus/send" //请求地址
-  let data = { "token": pushToken, "title": title, "content": content, "template": "markdown" } //将消息内容装入data
-  let headers = { "content-type": "application/json" }  //headers
-  let resp = HTTP.post(url, data, headers) //发送请求
-
-  if (resp.status !== 200) { throw new Error("err! status is " + resp.status()) }
-  let res = resp.json()  //返回json
-  // console.log(res)
-  return res
-}
-
-//邮件通知
-function sendEmail(userEmail, message) {
-  const sHost = emailSheet.Range("B1").Text  //发件箱host
-  const sPort = parseInt(emailSheet.Range("B2").Text)  //发件箱port
-  const sEmail = emailSheet.Range("B3").Text  //发件箱
-  const sPwd = emailSheet.Range("B4").Text  //发件箱SMTP授权码
-
-  try {
-    let mailer = SMTP.login({
-      host: sHost,
-      port: sPort,
-      username: sEmail,
-      password: sPwd,
-      secure: true
-    });
-    mailer.send({
-      from: "Bilibili签到<" + sEmail + ">",
-      to: userEmail,
-      subject: "Bilibili签到通知 [" + currentDate + "]",
-      text: message
-    });
-  } catch (error) { console.error(error) }
+/**
+ * 添加通知消息内容
+ * 传入参数：内容
+ */
+function addMsg(content) {
+  if (msgContent == '') {
+    msgContent = content
+  } else {
+    msgContent += ' \n' + content
+  }
 }
